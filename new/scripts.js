c2f0f6615c4ab622b59c741b04dec158cfd21487
Isaac Lyman <ilyman@mail.mtc.byu.edu>
2015-05-14 15:41:24 -0600

    Use jQuery

--- new/scripts.js
+++ new/scripts.js
@@ -1,55 +1,29 @@
 // ONLOAD
 
 window.onload = function () {
-	flipper.beginFlying();
-};
-
-// OBJECTS
-
-var il = {
-	talents: ['code', 'test', 'write', 'improve', 'think', 'learn', 'hike', 'run', 'laugh', 'work', 'impress'],
-	currentIndex: 0,
-	currentTalent: function() {
-		return this.talents[this.currentIndex];
-	},
-	nextTalent: function() {
-		this.currentIndex = this.currentIndex + 1 < this.talents.length ? this.currentIndex + 1 : 0;
-	}
-};
-
-var flipper = {
-	talentEl: null,
-	beginFlying: function() {
-		setInterval(this.flyTalents, 1000);
-	},
-	flyTalents: function() {
-		var talentEl = document.getElementById('flyup');
-		flyUp(talentEl, 50, 500);
-		// il.nextTalent();
-		talentEl.innerText = il.currentTalent();
-		// flyIn(talentEl, 50, 100);
-	}
+	var il = new Il();
+	il.beginFlying();
 };
 
 
-// TOOLBELT
+// CLASSES
 
-function flyUp(element, amount, timeout) {
-	console.log('add');
-	var moving = setInterval(function () {
-		moveUp(element, amount / 50);
-	}, timeout / 50);
-	// setTimeout(function () {
-	// 	clearInterval(moving);
-	// 	console.log('clear');
-	// }, timeout);
-}
-
-function flyIn(element, amount, timeout) {
-	element.style.top = parseInt(element.style.height) * 2 + 'px';
-	flyUp(element, amount, timeout);
-}
-
-function moveUp(element, pixels) {
-	element.style.top = parseInt(element.style.top) - pixels + 'px';
+function Il() {
+	var talents = ['code', 'test', 'write', 'improve', 'think', 'learn', 'run', 'laugh', 'work', 'impress', 'dance'];
+	var currentIndex = 0;
+	var nextTalent = function() {
+		currentIndex = currentIndex + 1 < talents.length ? currentIndex + 1 : 0;
+	};
+	var talentElement = $('#flyup');
+	var flyTalents = function() {
+		talentElement.fadeOut(200, function () {
+			nextTalent();
+			talentElement.text(talents[currentIndex]);
+		}).fadeIn(200);
+	};
+	
+	this.beginFlying = function() {
+		setInterval(flyTalents, 1200);
+	};
+	return this;
 }
