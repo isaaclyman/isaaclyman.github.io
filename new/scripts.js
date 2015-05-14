9ebe47d1c5c01d8be59de4ee2327e3446f29fb62
Isaac Lyman <ilyman@mail.mtc.byu.edu>
2015-05-14 12:35:12 -0600

    Scaffolding for flip function

--- new/scripts.js
+++ new/scripts.js
@@ -1,49 +1,55 @@
 // ONLOAD
 
-window.onload = initialize;
-
-function initialize() {
-	beginFlipping();
-}
-
-// VARIABLES
-
-var il = {
-	talents: ['code', 'test', 'write', 'think', 'learn', 'hike', 'run', 'win', 'improve']
+window.onload = function () {
+	flipper.beginFlying();
 };
 
-var flipper = {
-	talentEl: document.getElementById('fly-up'),
-	flipTalents: function() {
+// OBJECTS
 
+var il = {
+	talents: ['code', 'test', 'write', 'improve', 'think', 'learn', 'hike', 'run', 'laugh', 'work', 'impress'],
+	currentIndex: 0,
+	currentTalent: function() {
+		return this.talents[this.currentIndex];
 	},
-	waitThen: function(delay, fn) {
-		
+	nextTalent: function() {
+		this.currentIndex = this.currentIndex + 1 < this.talents.length ? this.currentIndex + 1 : 0;
 	}
 };
 
-// FUNCTIONS
-
-function beginFlipping() {
-
-}
+var flipper = {
+	talentEl: null,
+	beginFlying: function() {
+		setInterval(this.flyTalents, 1000);
+	},
+	flyTalents: function() {
+		var talentEl = document.getElementById('flyup');
+		flyUp(talentEl, 50, 500);
+		// il.nextTalent();
+		talentEl.innerText = il.currentTalent();
+		// flyIn(talentEl, 50, 100);
+	}
+};
 
 
 // TOOLBELT
 
-function waitThen(delay, function) {
-
+function flyUp(element, amount, timeout) {
+	console.log('add');
+	var moving = setInterval(function () {
+		moveUp(element, amount / 50);
+	}, timeout / 50);
+	// setTimeout(function () {
+	// 	clearInterval(moving);
+	// 	console.log('clear');
+	// }, timeout);
 }
 
-function hasClass(element, class) {
-	return element.className.match();
+function flyIn(element, amount, timeout) {
+	element.style.top = parseInt(element.style.height) * 2 + 'px';
+	flyUp(element, amount, timeout);
 }
 
-function addClass(element, class) {
-	element.className += ' ' + class;
-}
-
-function removeClass(element, class) {
-	var classRegExp = new RegExp('(?:^|\s)' + class + '(?!\S)', 'g');
-	element.className = element.className.replace(classRegExp, '');
+function moveUp(element, pixels) {
+	element.style.top = parseInt(element.style.top) - pixels + 'px';
 }
