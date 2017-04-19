38c2a7e4e1638921a248d7ecda9975705ab4bb11
Isaac Lyman <mail@isaaclyman.com>
2017-04-18 20:42:32 -0600

    Fix transition

--- sandbox/index.js
+++ sandbox/index.js
@@ -1,3 +1,5 @@
-var headshot = document.getElementById('headshot');
-headshot.classList.remove('offscreen');
-headshot.classList.add('onscreen');
+setTimeout(function () {
+  var headshot = document.getElementById('headshot');
+  headshot.classList.remove('offscreen');
+  headshot.classList.add('onscreen');
+}, 500);
